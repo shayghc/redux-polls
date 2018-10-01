913d9df1bb9c305297c7c4705f4ebe42d0c8b8c6
Seamus Connolly <sghconnolly@gmail.com>
2018-10-01 12:00:28 +0100

    chore: Troubleshoot errors

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,6 +3,8 @@ import { receiveUsers } from '../actions/users'
 import { receivePolls } from '../actions/polls'
 import { setAuthedUser } from '../actions/authedUser';
 
+const AUTHED_ID = 'tylermcginnis'
+
 export function handleInitialData() {
     return(dispatch) => {
         return getInitialData()
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-import middleware from './'
+import middleware from './middleware'
 
 const store = createStore(reducer, middleware)
 
